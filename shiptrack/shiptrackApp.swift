a398c5c1ef3676a2a49d9a26deae1d8507331600
Nick DeRose <nderose7@gmail.com>
2023-12-06 11:20:27 -0500

    Initial Commit

---
+++ shiptrack/shiptrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  shiptrackApp.swift
+//  shiptrack
+//
+//  Created by Nick DeRose on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct shiptrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
